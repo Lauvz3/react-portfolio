4dc933d633bf0388ca14f19e29a2e14abf292394
Hogan Guo <57429645+Lauvz3@users.noreply.github.com>
2024-05-07 02:51:35 -0700

    Updates

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UX Design</h3>
+              <p>
+                I'm a UX Designer with experience in creating lo-fi wireframes and hi-fi prototype
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UX Research</h3>
+              <p>
+                I have conduct multiple user research case studies to gain insights from the end users
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Graphic Design</h3>
+              <p>
+                I have designed icons and other graphics as well
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
